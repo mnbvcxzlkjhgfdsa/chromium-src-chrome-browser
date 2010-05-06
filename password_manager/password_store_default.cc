mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit 4252b4071f18c5045a12bee4d8f9029e69c0e970

Migrate web data service logins to the login database correctly on win.
BUG=43054
TEST=PasswordStoreWinTest.Migration

Review URL: http://codereview.chromium.org/1874001

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@46412 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

password_manager/password_store_default.cc
@@ -114,9 +114,14 @@ typedef std::vector<const PasswordForm*> PasswordForms;
 
 void PasswordStoreDefault::OnWebDataServiceRequestDone(
     WebDataService::Handle handle,
-    const WDTypedResult *result) {
+    const WDTypedResult* result) {
   DCHECK(handles_.end() != handles_.find(handle));
   DCHECK(result);
+
+  if (PASSWORD_RESULT != result->GetType()) {
+    NOTREACHED();
+    return;
+  }
 
   handles_.erase(handle);
   if (!result)

password_manager/password_store_default.h
@@ -43,13 +43,13 @@ class PasswordStoreDefault : public PasswordStore,
 
   scoped_refptr<WebDataService> web_data_service_;
 
- private:
-  // Migrates logins from the WDS to the LoginDatabase.
-  void MigrateIfNecessary();
-
   // Implements the WebDataService consumer interface.
   void OnWebDataServiceRequestDone(WebDataService::Handle handle,
                                    const WDTypedResult *result);
+
+ private:
+  // Migrates logins from the WDS to the LoginDatabase.
+  void MigrateIfNecessary();
 
   scoped_ptr<LoginDatabase> login_db_;
   Profile* profile_;

password_manager/password_store_win.cc
@@ -55,16 +55,17 @@ void PasswordStoreWin::NotifyConsumer(GetLoginsRequest* request,
 }
 
 void PasswordStoreWin::OnWebDataServiceRequestDone(
-    WebDataService::Handle handle, const WDTypedResult *result) {
-  scoped_ptr<GetLoginsRequest> request(TakeRequestWithHandle(handle));
-  // If the request was cancelled, we are done.
-  if (!request.get())
-    return;
-
+    WebDataService::Handle handle, const WDTypedResult* result) {
   if (!result)
     return;  // The WDS returns NULL if it is shutting down.
 
   if (PASSWORD_IE7_RESULT == result->GetType()) {
+    scoped_ptr<GetLoginsRequest> request(TakeRequestWithHandle(handle));
+
+    // If the request was cancelled, we are done.
+    if (!request.get())
+      return;
+
     // This is a response from WebDataService::GetIE7Login.
     PendingRequestFormMap::iterator it(pending_request_forms_.find(
         request->handle));
@@ -78,7 +79,7 @@ void PasswordStoreWin::OnWebDataServiceRequestDone(
     pending_request_forms_.erase(it);
     PasswordStore::NotifyConsumer(request.release(), forms);
   } else {
-    NOTREACHED();
+    PasswordStoreDefault::OnWebDataServiceRequestDone(handle, result);
   }
 }
 

password_manager/password_store_win_unittest.cc
@@ -23,6 +23,8 @@
 
 using base::WaitableEvent;
 using testing::_;
+using testing::DoAll;
+using testing::WithArg;
 using webkit_glue::PasswordForm;
 
 namespace {
@@ -51,6 +53,8 @@ class SignalingTask : public Task {
 };
 
 }  // anonymous namespace
+
+typedef std::vector<PasswordForm*> VectorOfForms;
 
 class PasswordStoreWinTest : public testing::Test {
  protected:
@@ -134,9 +138,18 @@ class PasswordStoreWinTest : public testing::Test {
   ScopedTempDir temp_dir_;
 };
 
+ACTION(STLDeleteElements0) {
+  STLDeleteContainerPointers(arg0.begin(), arg0.end());
+}
+
 ACTION(QuitUIMessageLoop) {
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
   MessageLoop::current()->Quit();
+}
+
+MATCHER(EmptyWDResult, "") {
+  return static_cast<const WDResult<std::vector<PasswordForm*> >*>(
+      arg)->GetValue().empty();
 }
 
 TEST_F(PasswordStoreWinTest, ConvertIE7Login) {
@@ -324,4 +337,166 @@ TEST_F(PasswordStoreWinTest, MultipleWDSQueriesOnDifferentThreads) {
   MessageLoop::current()->Run();
 
   STLDeleteElements(&forms);
+}
+
+TEST_F(PasswordStoreWinTest, Migration) {
+  PasswordFormData autofillable_data[] = {
+    { PasswordForm::SCHEME_HTML,
+      "http://foo.example.com",
+      "http://foo.example.com/origin",
+      "http://foo.example.com/action",
+      L"submit_element",
+      L"username_element",
+      L"password_element",
+      L"username_value",
+      L"password_value",
+      true, false, 1 },
+    { PasswordForm::SCHEME_HTML,
+      "http://bar.example.com",
+      "http://bar.example.com/origin",
+      "http://bar.example.com/action",
+      L"submit_element",
+      L"username_element",
+      L"password_element",
+      L"username_value",
+      L"password_value",
+      true, false, 2 },
+    { PasswordForm::SCHEME_HTML,
+      "http://baz.example.com",
+      "http://baz.example.com/origin",
+      "http://baz.example.com/action",
+      L"submit_element",
+      L"username_element",
+      L"password_element",
+      L"username_value",
+      L"password_value",
+      true, false, 3 },
+  };
+  PasswordFormData blacklisted_data[] = {
+    { PasswordForm::SCHEME_HTML,
+      "http://blacklisted.example.com",
+      "http://blacklisted.example.com/origin",
+      "http://blacklisted.example.com/action",
+      L"submit_element",
+      L"username_element",
+      L"password_element",
+      NULL,
+      NULL,
+      false, false, 1 },
+    { PasswordForm::SCHEME_HTML,
+      "http://blacklisted2.example.com",
+      "http://blacklisted2.example.com/origin",
+      "http://blacklisted2.example.com/action",
+      L"submit_element",
+      L"username_element",
+      L"password_element",
+      NULL,
+      NULL,
+      false, false, 2 },
+  };
+
+  VectorOfForms expected_autofillable;
+  for (unsigned int i = 0; i < ARRAYSIZE_UNSAFE(autofillable_data); ++i) {
+    expected_autofillable.push_back(
+        CreatePasswordFormFromData(autofillable_data[i]));
+  }
+
+  VectorOfForms expected_blacklisted;
+  for (unsigned int i = 0; i < ARRAYSIZE_UNSAFE(blacklisted_data); ++i) {
+    expected_blacklisted.push_back(
+        CreatePasswordFormFromData(blacklisted_data[i]));
+  }
+
+  // Populate the WDS with logins that should be migrated.
+  for (VectorOfForms::iterator it = expected_autofillable.begin();
+       it != expected_autofillable.end(); ++it) {
+    wds_->AddLogin(**it);
+  }
+  for (VectorOfForms::iterator it = expected_blacklisted.begin();
+       it != expected_blacklisted.end(); ++it) {
+    wds_->AddLogin(**it);
+  }
+
+  // The WDS schedules tasks to run on the DB thread so we schedule yet another
+  // task to notify us that it's safe to carry on with the test.
+  WaitableEvent done(false, false);
+  ChromeThread::PostTask(ChromeThread::DB, FROM_HERE, new SignalingTask(&done));
+  done.Wait();
+
+  // Initializing the PasswordStore should trigger a migration.
+  scoped_refptr<PasswordStoreWin> store(
+      new PasswordStoreWin(login_db_.release(), profile_.get(), wds_.get()));
+  store->Init();
+
+  // Check that the migration preference has not been initialized;
+  ASSERT_TRUE(NULL == profile_->GetPrefs()->FindPreference(
+      prefs::kLoginDatabaseMigrated));
+
+  // Again, the WDS schedules tasks to run on the DB thread, so schedule a task
+  // to signal us when it is safe to continue.
+  ChromeThread::PostTask(ChromeThread::DB, FROM_HERE, new SignalingTask(&done));
+  done.Wait();
+
+  // Let the WDS callbacks proceed so the logins can be migrated.
+  MessageLoop::current()->RunAllPending();
+
+  MockPasswordStoreConsumer consumer;
+
+  // Make sure we quit the MessageLoop even if the test fails.
+  ON_CALL(consumer, OnPasswordStoreRequestDone(_, _))
+      .WillByDefault(QuitUIMessageLoop());
+
+  // The autofillable forms should have been migrated from the WDS to the login
+  // database.
+  EXPECT_CALL(consumer,
+      OnPasswordStoreRequestDone(_,
+          ContainsAllPasswordForms(expected_autofillable)))
+      .WillOnce(DoAll(WithArg<1>(STLDeleteElements0()), QuitUIMessageLoop()));
+
+  store->GetAutofillableLogins(&consumer);
+  MessageLoop::current()->Run();
+
+  // The blacklisted forms should have been migrated from the WDS to the login
+  // database.
+  EXPECT_CALL(consumer,
+      OnPasswordStoreRequestDone(_,
+          ContainsAllPasswordForms(expected_blacklisted)))
+      .WillOnce(DoAll(WithArg<1>(STLDeleteElements0()), QuitUIMessageLoop()));
+
+  store->GetBlacklistLogins(&consumer);
+  MessageLoop::current()->Run();
+
+  // Check that the migration updated the migrated preference.
+  ASSERT_TRUE(profile_->GetPrefs()->GetBoolean(prefs::kLoginDatabaseMigrated));
+
+  MockWebDataServiceConsumer wds_consumer;
+
+  // No autofillable logins should be left in the WDS.
+  EXPECT_CALL(wds_consumer,
+      OnWebDataServiceRequestDone(_, EmptyWDResult()));
+
+  wds_->GetAutofillableLogins(&wds_consumer);
+
+  // Wait for the WDS methods to execute on the DB thread.
+  ChromeThread::PostTask(ChromeThread::DB, FROM_HERE, new SignalingTask(&done));
+  done.Wait();
+
+  // Handle the callback from the WDS.
+  MessageLoop::current()->RunAllPending();
+
+  // Likewise, no blacklisted logins should be left in the WDS.
+  EXPECT_CALL(wds_consumer,
+      OnWebDataServiceRequestDone(_, EmptyWDResult()));
+
+  wds_->GetBlacklistLogins(&wds_consumer);
+
+  // Wait for the WDS methods to execute on the DB thread.
+  ChromeThread::PostTask(ChromeThread::DB, FROM_HERE, new SignalingTask(&done));
+  done.Wait();
+
+  // Handle the callback from the WDS.
+  MessageLoop::current()->RunAllPending();
+
+  STLDeleteElements(&expected_autofillable);
+  STLDeleteElements(&expected_blacklisted);
 }

commit 0280dcf2dbd1da1d81afa60a9d2dc69da6ee1cc5

Fix a possible crash introduced by r46412 by not accessing the result variable before the NULL check
BUG=43408
TEST=none

Patch by Benjamin Jemlich <pcgod99@gmail.com>.

Review URL: http://codereview.chromium.org/2035003

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@46614 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

password_manager/password_store_default.cc
@@ -116,16 +116,15 @@ void PasswordStoreDefault::OnWebDataServiceRequestDone(
     WebDataService::Handle handle,
     const WDTypedResult* result) {
   DCHECK(handles_.end() != handles_.find(handle));
-  DCHECK(result);
+
+  handles_.erase(handle);
+  if (!result)
+    return;
 
   if (PASSWORD_RESULT != result->GetType()) {
     NOTREACHED();
     return;
   }
-
-  handles_.erase(handle);
-  if (!result)
-    return;
 
   const PasswordForms& forms =
       static_cast<const WDResult<PasswordForms>*>(result)->GetValue();