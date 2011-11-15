mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit 41a2d83271c16911591d9e25bbf8818110972c3f

Removed a couple of #ifdef TOUCH_UI and replaced with Widget::IsPureViews()
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/7253058

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@90978 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

ui/omnibox/omnibox_view_browsertest.cc
@@ -42,6 +42,7 @@
 #if defined(TOOLKIT_VIEWS)
 #include "views/controls/textfield/native_textfield_views.h"
 #include "views/events/event.h"
+#include "views/widget/widget.h"
 #endif
 
 using base::Time;
@@ -154,7 +155,7 @@ class OmniboxViewTest : public InProcessBrowserTest,
     ASSERT_NO_FATAL_FAILURE(SetupComponents());
     browser()->FocusLocationBar();
 #if defined(TOOLKIT_VIEWS)
-    if (views::NativeTextfieldViews::IsTextfieldViewsEnabled())
+    if (views::Widget::IsPureViews())
       return;
 #endif
     ASSERT_TRUE(ui_test_utils::IsViewFocused(browser(),
@@ -1334,7 +1335,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, FLAKY_PasteReplacingAll) {
 class OmniboxViewViewsTest : public OmniboxViewTest {
  public:
   OmniboxViewViewsTest() {
-    views::NativeTextfieldViews::SetEnableTextfieldViews(true);
+    views::Widget::IsPureViews();
   }
 };
 

ui/gtk/omnibox/omnibox_view_gtk.cc
@@ -881,7 +881,7 @@ OmniboxView* OmniboxViewGtk::Create(AutocompleteEditController* controller,
                                     CommandUpdater* command_updater,
                                     bool popup_window_mode,
                                     views::View* location_bar) {
-  if (views::NativeTextfieldViews::IsTextfieldViewsEnabled()) {
+  if (views::Widget::IsPureViews()) {
     OmniboxViewViews* omnibox_view = new OmniboxViewViews(controller,
                                                           toolbar_model,
                                                           profile,

ui/views/location_bar/location_bar_view.cc
@@ -173,7 +173,7 @@ void LocationBarView::Init() {
   // URL edit field.
   // View container for URL edit field.
 #if defined(OS_WIN)
-  if (UseViewsOmnibox()) {
+  if (views::Widget::IsPureViews()) {
     OmniboxViewViews* omnibox_view =
         new OmniboxViewViews(this, model_, profile_,
                              browser_->command_updater(), mode_ == POPUP, this);
@@ -431,7 +431,7 @@ void LocationBarView::SetInstantSuggestion(const string16& text,
           GetColor(ToolbarModel::NONE,
                    LocationBarView::DEEMPHASIZED_TEXT));
       suggested_text_view_->SetText(UTF16ToWide(text));
-      if (UseViewsOmnibox())
+      if (views::Widget::IsPureViews())
         NOTIMPLEMENTED();
       else
         suggested_text_view_->SetFont(GetOmniboxViewWin()->GetFont());
@@ -547,7 +547,7 @@ void LocationBarView::Layout() {
 
 #if defined(OS_WIN)
   int max_edit_width = entry_width;
-  if (UseViewsOmnibox()) {
+  if (views::Widget::IsPureViews()) {
     NOTIMPLEMENTED();
   } else {
     RECT formatting_rect;
@@ -675,7 +675,7 @@ void LocationBarView::Layout() {
   // keyword hints and suggested text is minimal and we're not confident this
   // is the right approach for suggested text.
   if (suggested_text_view_) {
-    if (UseViewsOmnibox()) {
+    if (views::Widget::IsPureViews()) {
       NOTIMPLEMENTED();
     } else {
       // TODO(sky): need to layout when the user changes caret position.
@@ -801,7 +801,7 @@ void LocationBarView::OnMouseReleased(const views::MouseEvent& event) {
 }
 
 void LocationBarView::OnMouseCaptureLost() {
-  if (UseViewsOmnibox())
+  if (views::Widget::IsPureViews())
     NOTIMPLEMENTED();
   else
     GetOmniboxViewWin()->HandleExternalMsg(WM_CAPTURECHANGED, 0, CPoint());
@@ -997,7 +997,7 @@ void LocationBarView::OnMouseEvent(const views::MouseEvent& event, UINT msg) {
   UINT flags = event.GetWindowsFlags();
   gfx::Point screen_point(event.location());
   ConvertPointToScreen(this, &screen_point);
-  if (UseViewsOmnibox())
+  if (views::Widget::IsPureViews())
     NOTIMPLEMENTED();
   else
     GetOmniboxViewWin()->HandleExternalMsg(msg, flags, screen_point.ToPOINT());
@@ -1032,7 +1032,7 @@ std::string LocationBarView::GetClassName() const {
 bool LocationBarView::SkipDefaultKeyEventProcessing(
     const views::KeyEvent& event) {
 #if defined(OS_WIN)
-  bool views_omnibox = UseViewsOmnibox();
+  bool views_omnibox = views::Widget::IsPureViews();
   if (views::FocusManager::IsTabTraversalKeyEvent(event)) {
     if (HasValidSuggestText()) {
       // Return true so that the edit sees the tab and commits the suggestion.
@@ -1256,12 +1256,7 @@ bool LocationBarView::HasValidSuggestText() const {
 }
 
 OmniboxViewWin* LocationBarView::GetOmniboxViewWin() {
-  CHECK(!UseViewsOmnibox());
+  CHECK(!views::Widget::IsPureViews());
   return static_cast<OmniboxViewWin*>(location_entry_.get());
-}
-
-bool LocationBarView::UseViewsOmnibox() {
-  return views::Widget::IsPureViews() ||
-      views::NativeTextfieldViews::IsTextfieldViewsEnabled();
 }
 #endif

ui/views/location_bar/location_bar_view.h
@@ -334,10 +334,6 @@ class LocationBarView : public LocationBar,
   // Returns |location_entry_| cast to OmniboxViewWin, or NULL if
   // |location_entry_| is of a different type.
   OmniboxViewWin* GetOmniboxViewWin();
-
-  // Returns true if the views-based omnibox should be used. When false,
-  // |location_entry_| can be cast to OmniboxViewWin.
-  static bool UseViewsOmnibox();
 #endif
 
   // Helper to show the first run info bubble.

commit 035d9e6bf84cf7aa3c7344a17d7069a536414eca

Turn on pure views for pure view omnibox tests looks like this is regressed in r90978
BUG=none
TEST=OmniboxViewViews test will use TextfieldViews


Review URL: http://codereview.chromium.org/8567009

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@110102 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

ui/omnibox/omnibox_view_browsertest.cc
@@ -1343,7 +1343,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, FLAKY_PasteReplacingAll) {
 class OmniboxViewViewsTest : public OmniboxViewTest {
  public:
   OmniboxViewViewsTest() {
-    views::Widget::IsPureViews();
+    views::Widget::SetPureViews(true);
   }
 };
 