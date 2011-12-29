mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit 4aa3f356870c141775dcf97edf2f1255a2f0acdd

Add more per-tab preferences.
intl.global.charset_default
webkit.webprefs.global.standard_font_family
webkit.webprefs.global.fixed_font_family
webkit.webprefs.global.serif_font_family
webkit.webprefs.global.sansserif_font_family
webkit.webprefs.global.cursive_font_family
webkit.webprefs.global.fantasy_font_family
webkit.webprefs.global.default_font_size
webkit.webprefs.global.default_fixed_font_size
webkit.webprefs.global.minimum_font_size
webkit.webprefs.global.minimum_logical_font_size
webkit.webprefs.global.javascript_can_open_windows_automatically
webkit.webprefs.global.loads_images_automatically
webkit.webprefs.global.plugins_enabled

BUG=105537
TEST=PrefsTabHelperTest*,PrefsTabHelperBrowserTest*

For files that has only prefs names changes and require OWNERS approval:

TBR=erikwright@chromium.org,gene@chromium.org,csilv@chromium.org,atwilson@chromium.org,mirandac@chromium.org

Review URL: http://codereview.chromium.org/8879016

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@115955 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

prefs/pref_service.cc
@@ -745,6 +745,24 @@ void PrefService::RegisterPreference(const char* path,
     pref_sync_associator_->RegisterPref(path);
 }
 
+void PrefService::UnregisterPreference(const char* path) {
+  DCHECK(CalledOnValidThread());
+
+  Preference p(this, path, Value::TYPE_NULL);
+  PreferenceSet::const_iterator it = prefs_.find(&p);
+  if (it == prefs_.end()) {
+    NOTREACHED() << "Trying to unregister an unregistered pref: " << path;
+    return;
+  }
+
+  prefs_.erase(it);
+  default_store_->RemoveDefaultValue(path);
+  if (pref_sync_associator_.get() &&
+      pref_sync_associator_->IsPrefRegistered(path)) {
+    pref_sync_associator_->UnregisterPref(path);
+  }
+}
+
 void PrefService::ClearPref(const char* path) {
   DCHECK(CalledOnValidThread());
 

automation/automation_provider.cc
@@ -730,7 +730,8 @@ void AutomationProvider::OnSetPageFontSize(int tab_handle,
       DCHECK(tab->tab_contents()->GetBrowserContext() != NULL);
       Profile* profile =
           Profile::FromBrowserContext(tab->tab_contents()->GetBrowserContext());
-      profile->GetPrefs()->SetInteger(prefs::kWebKitDefaultFontSize, font_size);
+      profile->GetPrefs()->SetInteger(
+          prefs::kWebKitGlobalDefaultFontSize, font_size);
     }
   }
 }

browser_encoding_uitest.cc
@@ -249,7 +249,7 @@ TEST_F(BrowserEncodingTest, MAYBE_TestEncodingAutoDetect) {
   // Set the default charset to one of encodings not supported by the current
   // auto-detector (Please refer to the above comments) to make sure we
   // incorrectly decode the page. Now we use ISO-8859-4.
-  ASSERT_TRUE(browser->SetStringPreference(prefs::kDefaultCharset,
+  ASSERT_TRUE(browser->SetStringPreference(prefs::kGlobalDefaultCharset,
                                            "ISO-8859-4"));
   scoped_refptr<TabProxy> tab(GetActiveTab());
   ASSERT_TRUE(tab.get());

net/chrome_url_request_context.cc
@@ -279,9 +279,9 @@ void ChromeURLRequestContextGetter::Observe(
               &ChromeURLRequestContextGetter::OnAcceptLanguageChange,
               this,
               accept_language));
-    } else if (*pref_name_in == prefs::kDefaultCharset) {
+    } else if (*pref_name_in == prefs::kGlobalDefaultCharset) {
       std::string default_charset =
-          prefs->GetString(prefs::kDefaultCharset);
+          prefs->GetString(prefs::kGlobalDefaultCharset);
       BrowserThread::PostTask(
           BrowserThread::IO, FROM_HERE,
           base::Bind(
@@ -308,7 +308,7 @@ void ChromeURLRequestContextGetter::RegisterPrefsObserver(Profile* profile) {
 
   registrar_.Init(profile->GetPrefs());
   registrar_.Add(prefs::kAcceptLanguages, this);
-  registrar_.Add(prefs::kDefaultCharset, this);
+  registrar_.Add(prefs::kGlobalDefaultCharset, this);
   registrar_.Add(prefs::kClearSiteDataOnExit, this);
 }
 

prefs/default_pref_store.cc
@@ -15,6 +15,11 @@ void DefaultPrefStore::SetDefaultValue(const std::string& key, Value* value) {
   SetValue(key, value);
 }
 
+void DefaultPrefStore::RemoveDefaultValue(const std::string& key) {
+  CHECK(GetValue(key, NULL) == READ_OK);
+  RemoveValue(key);
+}
+
 base::Value::Type DefaultPrefStore::GetType(const std::string& key) const {
   const Value* value;
   return GetValue(key, &value) == READ_OK ? value->GetType() : Value::TYPE_NULL;

prefs/default_pref_store.h
@@ -21,6 +21,9 @@ class DefaultPrefStore : public ValueMapPrefStore {
   // Stores a new |value| for |key|. Assumes ownership of |value|.
   void SetDefaultValue(const std::string& key, Value* value);
 
+  // Removes the value for |key|.
+  void RemoveDefaultValue(const std::string& key);
+
   // Returns the registered type for |key| or Value::TYPE_NULL if the |key|
   // has not been registered.
   base::Value::Type GetType(const std::string& key) const;

prefs/per_tab_user_pref_store.cc
@@ -8,5 +8,48 @@
 PerTabUserPrefStore::PerTabUserPrefStore(PersistentPrefStore* underlay)
     : OverlayUserPrefStore(underlay) {
   RegisterOverlayProperty(
-      prefs::kWebKitJavascriptEnabled, prefs::kWebKitGlobalJavascriptEnabled);
+      prefs::kWebKitJavascriptEnabled,
+      prefs::kWebKitGlobalJavascriptEnabled);
+  RegisterOverlayProperty(
+      prefs::kWebKitJavascriptCanOpenWindowsAutomatically,
+      prefs::kWebKitGlobalJavascriptCanOpenWindowsAutomatically);
+  RegisterOverlayProperty(
+      prefs::kWebKitLoadsImagesAutomatically,
+      prefs::kWebKitGlobalLoadsImagesAutomatically);
+  RegisterOverlayProperty(
+      prefs::kWebKitPluginsEnabled,
+      prefs::kWebKitGlobalPluginsEnabled);
+  RegisterOverlayProperty(
+      prefs::kDefaultCharset,
+      prefs::kGlobalDefaultCharset);
+  RegisterOverlayProperty(
+      prefs::kWebKitStandardFontFamily,
+      prefs::kWebKitGlobalStandardFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitFixedFontFamily,
+      prefs::kWebKitGlobalFixedFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitSerifFontFamily,
+      prefs::kWebKitGlobalSerifFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitSansSerifFontFamily,
+      prefs::kWebKitGlobalSansSerifFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitCursiveFontFamily,
+      prefs::kWebKitGlobalCursiveFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitFantasyFontFamily,
+      prefs::kWebKitGlobalFantasyFontFamily);
+  RegisterOverlayProperty(
+      prefs::kWebKitDefaultFontSize,
+      prefs::kWebKitGlobalDefaultFontSize);
+  RegisterOverlayProperty(
+      prefs::kWebKitDefaultFixedFontSize,
+      prefs::kWebKitGlobalDefaultFixedFontSize);
+  RegisterOverlayProperty(
+      prefs::kWebKitMinimumFontSize,
+      prefs::kWebKitGlobalMinimumFontSize);
+  RegisterOverlayProperty(
+      prefs::kWebKitMinimumLogicalFontSize,
+      prefs::kWebKitGlobalMinimumLogicalFontSize);
 }

prefs/pref_model_associator.cc
@@ -376,6 +376,11 @@ bool PrefModelAssociator::IsPrefRegistered(const char* name) {
   return registered_preferences_.count(name) > 0;
 }
 
+void PrefModelAssociator::UnregisterPref(const char* name) {
+  DCHECK(synced_preferences_.count(name) == 0);
+  registered_preferences_.erase(name);
+}
+
 void PrefModelAssociator::ProcessPrefChange(const std::string& name) {
   if (processing_syncer_changes_)
     return;  // These are changes originating from us, ignore.

commit 1a40e8bf03b7f28aab44ee718548137a64a49e43

Fix a memory leak introduced in r115955.
Forgot to delete the Preference object in PrefService::UnregisterPreference.

TBR=bauerb@chromium.org
BUG=none
TEST=no leaks from MigratePreferences

Review URL: http://codereview.chromium.org/9034018

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@115958 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

prefs/pref_service.cc
@@ -755,6 +755,7 @@ void PrefService::UnregisterPreference(const char* path) {
     return;
   }
 
+  delete *it;
   prefs_.erase(it);
   default_store_->RemoveDefaultValue(path);
   if (pref_sync_associator_.get() &&