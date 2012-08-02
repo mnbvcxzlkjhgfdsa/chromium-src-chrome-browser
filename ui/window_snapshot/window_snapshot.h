03f948202e97aacee572d95aa9bfa0bf2c1f1930
qfel@google.com <qfel@google.com@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2012-08-02 09:52:24 +0000

    Support calling GrabWindowSnapshot when no BrowserProcess instance is avaliable (eg. tests)
    BUG=139694
    
    
    Review URL: https://chromiumcodereview.appspot.com/10821104
    
    git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@149604 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- ui/window_snapshot/window_snapshot.h
+++ ui/window_snapshot/window_snapshot.h
@@ -20,7 +20,11 @@ namespace chrome {
 // Grabs a snapshot of the rectangle area |snapshot_bounds| with respect to the
 // top left corner of the designated window and stores a PNG representation
 // into a byte vector. On Windows, |window| may be NULL to grab a snapshot of
-// the primary monitor. Returns true if the operation is successful.
+// the primary monitor. This checks policy settings if taking screenshots is
+// allowed, and is intended to by used by browser code. If you need to take a
+// screenshot for debugging purposes, use ui::GrabWindowSnapshot
+// (ui/window_snapshot/window_snapshot.h).
+// Returns true if the operation is successful (ie. permitted).
 bool GrabWindowSnapshot(
     gfx::NativeWindow window,
     std::vector<unsigned char>* png_representation,
