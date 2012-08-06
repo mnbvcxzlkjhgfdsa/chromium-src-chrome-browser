mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit a7ab83ee5f56d9ef5df21a34fc4bc18c647bfe23

Change return type of FaviconTabHelper::GetFavicon() to gfx::Image.
No intended behavior change for now.
Depends on http://codereview.chromium.org/10832128/
Part of supporting hidpi favicons in the tab strip.

BUG=138550
TBR=joi

Review URL: https://chromiumcodereview.appspot.com/10837090

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@149891 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

ui/cocoa/tab_contents/favicon_util_mac.mm
@@ -19,9 +19,7 @@ namespace mac {
 
 NSImage* FaviconForTabContents(TabContents* contents) {
   if (contents && contents->favicon_tab_helper()->FaviconIsValid()) {
-    CGColorSpaceRef color_space = base::mac::GetSystemColorSpace();
-    NSImage* image = gfx::SkBitmapToNSImageWithColorSpace(
-        contents->favicon_tab_helper()->GetFavicon(), color_space);
+    NSImage* image = contents->favicon_tab_helper()->GetFavicon().ToNSImage();
     // The |image| could be nil if the bitmap is null. In that case, fallback
     // to the default image.
     if (image) {

favicon/favicon_tab_helper.cc
@@ -49,18 +49,18 @@ void FaviconTabHelper::FetchFavicon(const GURL& url) {
     touch_icon_handler_->FetchFavicon(url);
 }
 
-SkBitmap FaviconTabHelper::GetFavicon() const {
+gfx::Image FaviconTabHelper::GetFavicon() const {
   // Like GetTitle(), we also want to use the favicon for the last committed
   // entry rather than a pending navigation entry.
   const NavigationController& controller = web_contents()->GetController();
   NavigationEntry* entry = controller.GetTransientEntry();
   if (entry)
-    return entry->GetFavicon().AsBitmap();
+    return entry->GetFavicon().image;
 
   entry = controller.GetLastCommittedEntry();
   if (entry)
-    return entry->GetFavicon().AsBitmap();
-  return SkBitmap();
+    return entry->GetFavicon().image;
+  return gfx::Image();
 }
 
 bool FaviconTabHelper::FaviconIsValid() const {
@@ -112,7 +112,8 @@ void FaviconTabHelper::SaveFavicon() {
   }
   std::vector<unsigned char> image_data;
   // TODO: Save all representations.
-  gfx::PNGCodec::EncodeBGRASkBitmap(favicon.AsBitmap(), false, &image_data);
+  gfx::PNGCodec::EncodeBGRASkBitmap(
+      favicon.image.AsBitmap(), false, &image_data);
   service->SetFavicon(
       entry->GetURL(), favicon.url, image_data, history::FAVICON);
 }

favicon/favicon_tab_helper.h
@@ -15,6 +15,10 @@
 #include "content/public/browser/web_contents_observer.h"
 #include "googleurl/src/gurl.h"
 
+namespace gfx {
+class Image;
+}
+
 class FaviconHandler;
 class SkBitmap;
 
@@ -38,9 +42,9 @@ class FaviconTabHelper : public content::WebContentsObserver,
 
   // Returns the favicon for this tab, or IDR_DEFAULT_FAVICON if the tab does
   // not have a favicon. The default implementation uses the current navigation
-  // entry. This will return an isNull bitmap if there are no navigation
+  // entry. This will return an empty bitmap if there are no navigation
   // entries, which should rarely happen.
-  SkBitmap GetFavicon() const;
+  gfx::Image GetFavicon() const;
 
   // Returns true if we have the favicon for the page.
   bool FaviconIsValid() const;

instant/instant_loader.cc
@@ -379,8 +379,8 @@ void InstantLoader::WebContentsDelegateImpl::CommitHistory(
       !active_entry->GetFavicon().image.IsEmpty()) {
     std::vector<unsigned char> image_data;
     // TODO: Add all variants once the history service supports it.
-    gfx::PNGCodec::EncodeBGRASkBitmap(active_entry->GetFavicon().AsBitmap(),
-                                      false, &image_data);
+    gfx::PNGCodec::EncodeBGRASkBitmap(
+        active_entry->GetFavicon().image.AsBitmap(), false, &image_data);
     favicon_service->SetFavicon(active_entry->GetURL(),
                                 active_entry->GetFavicon().url,
                                 image_data,

task_manager/task_manager_resource_providers.cc
@@ -334,7 +334,7 @@ string16 TaskManagerTabContentsResource::GetProfileName() const {
 gfx::ImageSkia TaskManagerTabContentsResource::GetIcon() const {
   if (IsPrerendering())
     return *prerender_icon_;
-  return tab_contents_->favicon_tab_helper()->GetFavicon();
+  return tab_contents_->favicon_tab_helper()->GetFavicon().AsImageSkia();
 }
 
 WebContents* TaskManagerTabContentsResource::GetWebContents() const {

ui/browser.cc
@@ -534,7 +534,9 @@ SkBitmap Browser::GetCurrentPageIcon() const {
   TabContents* contents = chrome::GetActiveTabContents(this);
   // |contents| can be NULL since GetCurrentPageIcon() is called by the window
   // during the window's creation (before tabs have been added).
-  return contents ? contents->favicon_tab_helper()->GetFavicon() : SkBitmap();
+  // TODO: Let this return a gfx::Image.
+  return contents ?
+      contents->favicon_tab_helper()->GetFavicon().AsBitmap() : SkBitmap();
 }
 
 string16 Browser::GetWindowTitleForCurrentTab() const {

ui/content_settings/content_setting_bubble_model.cc
@@ -468,7 +468,8 @@ void ContentSettingPopupBubbleModel::SetPopups() {
       title = l10n_util::GetStringUTF8(IDS_TAB_LOADING_TITLE);
     PopupItem popup_item;
     popup_item.title = title;
-    popup_item.bitmap = (*i)->favicon_tab_helper()->GetFavicon();
+    // TODO: Make this use gfx::Image.
+    popup_item.bitmap = (*i)->favicon_tab_helper()->GetFavicon().AsBitmap();
     popup_item.tab_contents = (*i);
     add_popup(popup_item);
   }

ui/gtk/hung_renderer_dialog_gtk.cc
@@ -191,7 +191,7 @@ void HungRendererDialogGtk::ShowForWebContents(WebContents* hung_contents) {
       std::string title = UTF16ToUTF8(it->web_contents()->GetTitle());
       if (title.empty())
         title = UTF16ToUTF8(CoreTabHelper::GetDefaultTitle());
-      SkBitmap favicon = it->favicon_tab_helper()->GetFavicon();
+      SkBitmap favicon = it->favicon_tab_helper()->GetFavicon().AsBitmap();
 
       GdkPixbuf* pixbuf = NULL;
       if (favicon.width() > 0)

commit 3079c34ed0c700193a74d6c8fadfff406f36474a

Fall back to the default favicon on invalid favicon files.
I regressed this in r149891.

BUG=140689

Review URL: https://chromiumcodereview.appspot.com/10853018

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@150092 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

ui/cocoa/tab_contents/favicon_util_mac.mm
@@ -13,7 +13,7 @@ namespace mac {
 
 NSImage* FaviconForTabContents(TabContents* contents) {
   if (contents && contents->favicon_tab_helper()->FaviconIsValid()) {
-    NSImage* image = contents->favicon_tab_helper()->GetFavicon().ToNSImage();
+    NSImage* image = contents->favicon_tab_helper()->GetFavicon().AsNSImage();
     // The |image| could be nil if the bitmap is null. In that case, fallback
     // to the default image.
     if (image) {