mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit 0eb381c92baed0c272fb149eeb2118982212a0d5

Make TabContents ctor private; poke hole for existing callers.
BUG=107201
TEST=no change


Review URL: https://chromiumcodereview.appspot.com/10868072

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@153527 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

ui/tab_contents/tab_contents.h
@@ -16,18 +16,33 @@ class AutocompleteHistoryManager;
 class AutofillManager;
 class AutofillExternalDelegate;
 class AutomationTabHelper;
+class BasePanelBrowserTest;
 class BlockedContentTabHelper;
 class BookmarkTabHelper;
+class Browser;
+class BrowserCommandsTabContentsCreator;
+class BrowserLauncherItemControllerContentsCreator;
+class BrowserTabstripTabContentsCreator;
+class ChromeWebContentsHandler;
+class ConstrainedWebDialogDelegateBase;
 class ConstrainedWindowTabHelper;
 class CoreTabHelper;
+class ExtensionTabUtil;
 class ExternalProtocolObserver;
+class ExternalTabContainerWin;
 class FaviconTabHelper;
+class FindBackendTestContentsCreator;
 class FindTabHelper;
+class GeolocationPermissionContextTests;
 class HistoryTabHelper;
 class HungPluginTabHelper;
+class InfoBarControllerContentsCreator;
 class InfoBarTabHelper;
+class InstantLoader;
 class MetroPinTabHelper;
 class NavigationMetricsRecorder;
+class OffscreenTabContentsCreator;
+class OldBasePanelBrowserTest;
 class OmniboxSearchHint;
 class PasswordManager;
 class PasswordManagerDelegate;
@@ -38,12 +53,23 @@ class Profile;
 class RestoreTabHelper;
 class SadTabHelper;
 class SearchEngineTabHelper;
+class ShellWindow;
 class SnapshotTabHelper;
 class TabContentsSSLHelper;
+class TabContentsTestHarness;
 class TabSpecificContentSettings;
+class TabStripModel;
+class TabStripModelContentsCreator;
 class ThumbnailGenerator;
 class TranslateTabHelper;
+class TranslationInfoBarTestContentsCreator;
+class WebDialogGtk;
+class WebDialogWindowControllerTabContentsCreator;
+class WebIntentInlineDispositionBrowserTest;
+class WebIntentPickerCocoa;
 class WebIntentPickerController;
+class WebIntentPickerGtk;
+class WebUITestContentsCreator;
 class ZoomController;
 
 #if defined(ENABLE_ONE_CLICK_SIGNIN)
@@ -68,12 +94,19 @@ namespace chrome_browser_net {
 class LoadTimeStatsTabHelper;
 }
 
+namespace chromeos {
+class SimpleWebViewDialog;
+class WebUILoginView;
+}
+
 namespace extensions {
 class TabHelper;
+class WebAuthFlow;
 class WebNavigationTabObserver;
 }
 
 namespace prerender {
+class PrerenderContents;
 class PrerenderTabHelper;
 }
 
@@ -103,14 +136,48 @@ class SafeBrowsingTabObserver;
 // least to make easy for other WebContents hosts to include and support.
 class TabContents : public content::WebContentsObserver {
  public:
-  // Takes ownership of |contents|, which must be heap-allocated (as it lives
-  // in a scoped_ptr) and can not be NULL.
-  explicit TabContents(content::WebContents* contents);
-  virtual ~TabContents();
+  class Factory {
+   private:
+    // TabContents is going away <http://crbug.com/107201> so don't allow any
+    // more code to construct instances. Explicitly befriend those who currently
+    // do so.
 
-  // Create a TabContents with the same state as this one. The returned
-  // heap-allocated pointer is owned by the caller.
-  TabContents* Clone();
+    friend class BasePanelBrowserTest;
+    friend class Browser;
+    friend class BrowserCommandsTabContentsCreator;
+    friend class BrowserLauncherItemControllerContentsCreator;
+    friend class BrowserTabstripTabContentsCreator;
+    friend class chromeos::SimpleWebViewDialog;
+    friend class chromeos::WebUILoginView;
+    friend class ChromeWebContentsHandler;
+    friend class ConstrainedWebDialogDelegateBase;
+    friend class extensions::WebAuthFlow;
+    friend class ExtensionTabUtil;
+    friend class ExternalTabContainerWin;
+    friend class FindBackendTestContentsCreator;
+    friend class GeolocationPermissionContextTests;
+    friend class InfoBarControllerContentsCreator;
+    friend class InstantLoader;
+    friend class OldBasePanelBrowserTest;
+    friend class OffscreenTabContentsCreator;
+    friend class prerender::PrerenderContents;
+    friend class ShellWindow;
+    friend class TabContentsTestHarness;
+    friend class TabStripModel;
+    friend class TabStripModelContentsCreator;
+    friend class TranslationInfoBarTestContentsCreator;
+    friend class WebDialogGtk;
+    friend class WebDialogWindowControllerTabContentsCreator;
+    friend class WebIntentInlineDispositionBrowserTest;
+    friend class WebIntentPickerCocoa;
+    friend class WebIntentPickerGtk;
+    friend class WebUITestContentsCreator;
+
+    static TabContents* CreateTabContents(content::WebContents* contents);
+    static TabContents* CloneTabContents(TabContents* contents);
+  };
+
+  virtual ~TabContents();
 
   // Helper to retrieve the existing instance that owns a given WebContents.
   // Returns NULL if there is no such existing instance.
@@ -257,6 +324,16 @@ class TabContents : public content::WebContentsObserver {
   virtual void UserAgentOverrideSet(const std::string& user_agent) OVERRIDE;
 
  private:
+  friend class TabContentsFactory;
+
+  // Takes ownership of |contents|, which must be heap-allocated (as it lives
+  // in a scoped_ptr) and can not be NULL.
+  explicit TabContents(content::WebContents* contents);
+
+  // Create a TabContents with the same state as this one. The returned
+  // heap-allocated pointer is owned by the caller.
+  TabContents* Clone();
+
   // Tab Helpers ---------------------------------------------------------------
   // (These provide API for callers and have a getter function listed in the
   // "Tab Helpers" section in the member functions area, above.)

chromeos/login/simple_web_view_dialog.cc
@@ -151,7 +151,7 @@ void SimpleWebViewDialog::StartLoad(const GURL& url) {
                           NULL,
                           MSG_ROUTING_NONE,
                           NULL);
-  tab_contents_.reset(new TabContents(web_contents));
+  tab_contents_.reset(TabContents::Factory::CreateTabContents(web_contents));
   web_view_->SetWebContents(web_contents);
   web_contents->SetDelegate(this);
   web_view_->LoadInitialURL(url);

chromeos/login/webui_login_view.cc
@@ -154,7 +154,7 @@ void WebUILoginView::Init(views::Widget* login_window) {
                           NULL,
                           MSG_ROUTING_NONE,
                           NULL);
-  tab_contents_.reset(new TabContents(web_contents));
+  tab_contents_.reset(TabContents::Factory::CreateTabContents(web_contents));
   webui_login_->SetWebContents(web_contents);
 
   web_contents->SetDelegate(this);

extensions/api/identity/web_auth_flow.cc
@@ -108,7 +108,7 @@ WebContents* WebAuthFlow::CreateWebContents() {
 
 void WebAuthFlow::ShowAuthFlowPopup() {
   // Pass ownership of WebContents to TabContents.
-  tab_contents_ = new TabContents(contents_);
+  tab_contents_ = TabContents::Factory::CreateTabContents(contents_);
   contents_ = NULL;
   Browser* browser = new Browser(Browser::CreateParams(
       Browser::TYPE_POPUP, profile_));

extensions/api/offscreen_tabs/offscreen_tabs_api.cc
@@ -52,6 +52,14 @@ namespace keys = extensions::offscreen_tabs_constants;
 namespace tabs_keys = extensions::tabs_constants;
 namespace events = extensions::event_names;
 
+// TODO(avi): Kill this when TabContents goes away.
+class OffscreenTabContentsCreator {
+ public:
+  static TabContents* CreateTabContents(content::WebContents* contents) {
+    return TabContents::Factory::CreateTabContents(contents);
+  }
+};
+
 namespace {
 
 class ParentTab;
@@ -228,7 +236,8 @@ void OffscreenTab::Init(const GURL& url,
   // Create the offscreen tab.
   WebContents* web_contents = WebContents::Create(
       profile, NULL, MSG_ROUTING_NONE, NULL);
-  tab_contents_.reset(new TabContents(web_contents));
+  tab_contents_.reset(
+      OffscreenTabContentsCreator::CreateTabContents(web_contents));
 
   // Setting the size starts the renderer.
   SetSize(width, height);

extensions/extension_tab_util.cc
@@ -232,7 +232,8 @@ void ExtensionTabUtil::CreateTab(WebContents* web_contents,
   const bool browser_created = !browser;
   if (!browser)
     browser = new Browser(Browser::CreateParams(profile));
-  TabContents* tab_contents = new TabContents(web_contents);
+  TabContents* tab_contents =
+      TabContents::Factory::CreateTabContents(web_contents);
   chrome::NavigateParams params(browser, tab_contents);
 
   // The extension_app_id parameter ends up as app_name in the Browser

geolocation/chrome_geolocation_permission_context_unittest.cc
@@ -216,7 +216,7 @@ void GeolocationPermissionContextTests::AddNewTab(const GURL& url) {
       url, content::Referrer(), content::PAGE_TRANSITION_TYPED, std::string());
   RenderViewHostTester::For(new_tab->GetRenderViewHost())->
       SendNavigate(extra_tabs_.size() + 1, url);
-  extra_tabs_.push_back(new TabContents(new_tab));
+  extra_tabs_.push_back(TabContents::Factory::CreateTabContents(new_tab));
 }
 
 void GeolocationPermissionContextTests::CheckTabContentsState(

instant/instant_loader.cc
@@ -264,10 +264,11 @@ InstantLoader::InstantLoader(InstantLoaderDelegate* delegate,
                              const TabContents* tab_contents)
     : loader_delegate_(delegate),
       preview_contents_(
-          new TabContents(content::WebContents::CreateWithSessionStorage(
-            tab_contents->profile(), NULL, MSG_ROUTING_NONE,
-            tab_contents->web_contents(),
-            tab_contents->web_contents()->GetController().
+          TabContents::Factory::CreateTabContents(
+              content::WebContents::CreateWithSessionStorage(
+                  tab_contents->profile(), NULL, MSG_ROUTING_NONE,
+                  tab_contents->web_contents(),
+                  tab_contents->web_contents()->GetController().
                                           GetSessionStorageNamespaceMap()))),
       supports_instant_(false),
       instant_url_(instant_url) {

commit 00871c701f261a311873a055018ce5b4f8ead4ae

Fix the android_webview build.
This is fixes the build break introduced in r153527.

BUG=


Review URL: https://chromiumcodereview.appspot.com/10889006

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@153699 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

ui/tab_contents/tab_contents.h
@@ -13,8 +13,8 @@
 
 class AlternateErrorPageTabObserver;
 class AutocompleteHistoryManager;
-class AutofillManager;
 class AutofillExternalDelegate;
+class AutofillManager;
 class AutomationTabHelper;
 class BasePanelBrowserTest;
 class BlockedContentTabHelper;
@@ -75,6 +75,10 @@ class ZoomController;
 #if defined(ENABLE_ONE_CLICK_SIGNIN)
 class OneClickSigninHelper;
 #endif
+
+namespace android_webview {
+class AwBrowserDependencyFactoryImpl;
+}
 
 namespace browser_sync {
 class SyncedTabDelegate;
@@ -142,6 +146,7 @@ class TabContents : public content::WebContentsObserver {
     // more code to construct instances. Explicitly befriend those who currently
     // do so.
 
+    friend class android_webview::AwBrowserDependencyFactoryImpl;
     friend class BasePanelBrowserTest;
     friend class Browser;
     friend class BrowserCommandsTabContentsCreator;