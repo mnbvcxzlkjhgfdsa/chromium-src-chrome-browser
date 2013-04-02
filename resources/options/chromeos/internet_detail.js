mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit c1eeee2c5fae8fcc6fb9d3335d139c80310ea71d

Display an info banner on the clear browsing data dialog if the browsing data was cleared within the last 24h. The displayed info banner suggest to use the incognito mode instead.
BUG=164600


Review URL: https://chromiumcodereview.appspot.com/12092089

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@180372 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

resources/options/clear_browser_data_overlay.css
@@ -20,3 +20,13 @@
 #flash-storage-settings {
   padding-top: 5px;
 }
+
+#info-banner {
+  background-color: rgb(249, 237, 184);
+  border: solid 1px rgb(237, 201, 103);
+  border-radius: 2px;
+  font-weight: bold;
+  margin: 0 20px 11px 17px;
+  padding: 6px 8px;
+}
+

resources/options/clear_browser_data_overlay.html
@@ -1,6 +1,9 @@
 <div id="clear-browser-data-overlay" class="page" hidden>
   <div class="close-button"></div>
   <h1 i18n-content="clearBrowserDataOverlay"></h1>
+  <div id="info-banner" hidden>
+    <span i18n-content="clearBrowserDataInfoBar"></span>
+  </div>
   <div id="cbd-content-area" class="content-area">
     <span i18n-content="clearBrowserDataLabel"></span>
     <select id="clear-browser-data-time-period"

resources/options/clear_browser_data_overlay.js
@@ -95,6 +95,11 @@ cr.define('options', function() {
       ClearBrowserDataOverlay.getInstance().updateCommitButtonState_();
   };
 
+  ClearBrowserDataOverlay.setBannerVisibility = function(args) {
+    var visible = args[0];
+    $('info-banner').hidden = !visible;
+  };
+
   ClearBrowserDataOverlay.doneClearing = function() {
     // The delay gives the user some feedback that the clearing
     // actually worked. Otherwise the dialog just vanishes instantly in most

ui/browser_ui_prefs.cc
@@ -68,6 +68,9 @@ void RegisterBrowserUserPrefs(PrefServiceSyncable* prefs) {
   prefs->RegisterIntegerPref(prefs::kDeleteTimePeriod,
                              0,
                              PrefServiceSyncable::SYNCABLE_PREF);
+  prefs->RegisterInt64Pref(prefs::kLastClearBrowsingDataTime,
+                           0,
+                           PrefServiceSyncable::UNSYNCABLE_PREF);
   prefs->RegisterBooleanPref(prefs::kCheckDefaultBrowser,
                              true,
                              PrefServiceSyncable::UNSYNCABLE_PREF);

ui/webui/options/clear_browser_data_handler.cc
@@ -21,6 +21,7 @@
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
+#include "ui/base/accelerators/accelerator.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -44,6 +45,25 @@ void ClearBrowserDataHandler::InitializeHandler() {
                                       Profile::FromWebUI(web_ui())->GetPrefs());
   pepper_flash_settings_enabled_.Init(prefs::kPepperFlashSettingsEnabled,
                                       Profile::FromWebUI(web_ui())->GetPrefs());
+}
+
+void ClearBrowserDataHandler::InitializePage() {
+  UpdateInfoBannerVisibility();
+}
+
+void ClearBrowserDataHandler::UpdateInfoBannerVisibility() {
+  Profile* profile = Profile::FromWebUI(web_ui());
+  base::Time lastClearBrowsingDataTime = base::Time::FromInternalValue(
+      profile->GetPrefs()->GetInt64(prefs::kLastClearBrowsingDataTime));
+
+  const int64 kHoursPerDay = 24;
+  bool visible = (base::Time::Now() - lastClearBrowsingDataTime) <=
+      base::TimeDelta::FromHours(kHoursPerDay);
+
+  ListValue args;
+  args.Append(Value::CreateBooleanValue(visible));
+  web_ui()->CallJavascriptFunction(
+      "ClearBrowserDataOverlay.setBannerVisibility", args);
 }
 
 void ClearBrowserDataHandler::GetLocalizedValues(
@@ -73,6 +93,13 @@ void ClearBrowserDataHandler::GetLocalizedValues(
       "clearBrowsingDataLearnMoreUrl",
       google_util::StringAppendGoogleLocaleParam(
           kClearBrowsingDataLearnMoreUrl));
+
+  ui::Accelerator acc(ui::VKEY_N, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
+  localized_strings->SetString(
+      "clearBrowserDataInfoBar",
+      l10n_util::GetStringFUTF16(
+          IDS_CLEAR_BROWSING_DATA_INFO_BAR_TEXT,
+          acc.GetShortcutText()));
 
   ListValue* time_list = new ListValue;
   for (int i = 0; i < 5; i++) {
@@ -152,6 +179,12 @@ void ClearBrowserDataHandler::HandleClearBrowserData(const ListValue* value) {
       static_cast<BrowsingDataRemover::TimePeriod>(period_selected));
   remover_->AddObserver(this);
   remover_->Remove(remove_mask, origin_mask);
+
+  // Store the clear browsing data time. Next time the clear browsing data
+  // dialog is open, this time is used to decide whether to display an info
+  // banner or not.
+  prefs->SetInt64(prefs::kLastClearBrowsingDataTime,
+                  base::Time::Now().ToInternalValue());
 }
 
 void ClearBrowserDataHandler::OnBrowsingDataRemoverDone() {

ui/webui/options/clear_browser_data_handler.h
@@ -21,9 +21,12 @@ class ClearBrowserDataHandler : public OptionsPageUIHandler,
   // OptionsPageUIHandler implementation.
   virtual void GetLocalizedValues(DictionaryValue* localized_strings) OVERRIDE;
   virtual void InitializeHandler() OVERRIDE;
+  virtual void InitializePage() OVERRIDE;
 
   // WebUIMessageHandler implementation.
   virtual void RegisterMessages() OVERRIDE;
+
+  void UpdateInfoBannerVisibility();
 
  private:
   // Javascript callback to start clearing data.

commit 26184269a5e3bd15810ffc8109e0ed73fc0b3f3b

Settings page: The id 'info-bar' was used for two HTMLElements.
Resolving the conflict by prefixing the id with 'network-proxy-' and
'clear-browser-data-', respectively.

This conflict and bug was introduced with r180372.

BUG=224595


Review URL: https://chromiumcodereview.appspot.com/13155002

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@191856 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

resources/options/chromeos/internet_detail.html
@@ -535,7 +535,7 @@
     </div>
     <div id="network-proxy-tab" class="subpages-tab-contents">
       <section>
-        <div id="info-banner" hidden>
+        <div id="network-proxy-info-banner" hidden>
           <span id="banner-text" class="page-banner-text"></span>
         </div>
         <div class="radio">

resources/options/chromeos/internet_detail.js
@@ -399,7 +399,7 @@ cr.define('options.internet', function() {
      * @private
      */
     updateProxyBannerVisibility_: function() {
-      var bannerDiv = $('info-banner');
+      var bannerDiv = $('network-proxy-info-banner');
       // Show banner and determine its message if necessary.
       var controlledBy = $('direct-proxy').controlledBy;
       if (!controlledBy || controlledBy == '') {

resources/options/clear_browser_data_overlay.css
@@ -21,7 +21,7 @@
   padding-top: 5px;
 }
 
-#info-banner {
+#clear-browser-data-info-banner {
   background-color: rgb(249, 237, 184);
   border: solid 1px rgb(237, 201, 103);
   border-radius: 2px;
@@ -29,4 +29,3 @@
   margin: 0 20px 11px 17px;
   padding: 6px 8px;
 }
-

resources/options/clear_browser_data_overlay.html
@@ -1,7 +1,7 @@
 <div id="clear-browser-data-overlay" class="page" hidden>
   <div class="close-button"></div>
   <h1 i18n-content="clearBrowserDataOverlay"></h1>
-  <div id="info-banner" hidden>
+  <div id="clear-browser-data-info-banner" hidden>
     <span i18n-content="clearBrowserDataInfoBar"></span>
   </div>
   <div id="cbd-content-area" class="content-area">

resources/options/clear_browser_data_overlay.js
@@ -125,7 +125,7 @@ cr.define('options', function() {
 
   ClearBrowserDataOverlay.setBannerVisibility = function(args) {
     var visible = args[0];
-    $('info-banner').hidden = !visible;
+    $('clear-browser-data-info-banner').hidden = !visible;
   };
 
   ClearBrowserDataOverlay.doneClearing = function() {