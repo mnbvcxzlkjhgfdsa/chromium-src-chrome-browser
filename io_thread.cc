mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit afd713422e2cdf48c885ce4e455f54051d04255c

Removed static factories for data, ftp, file, and about jobs. Instead add corresponding ProtocolHandlers as needed. Remove URLRequestContext members used by these static factories.  Bake FtpAuthCache into FtpProtocolHandler as it was already unique per FtpProtocolHandler. This is a revived version of http://crrev.com/10836206
BUG=142945


Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=188912

Review URL: https://chromiumcodereview.appspot.com/11931024

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@198915 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

io_thread.cc
@@ -50,7 +50,6 @@
 #include "net/dns/host_cache.h"
 #include "net/dns/host_resolver.h"
 #include "net/dns/mapped_host_resolver.h"
-#include "net/ftp/ftp_network_layer.h"
 #include "net/http/http_auth_filter.h"
 #include "net/http/http_auth_handler_factory.h"
 #include "net/http/http_network_layer.h"
@@ -189,8 +188,6 @@ ConstructProxyScriptFetcherContext(IOThread::Globals* globals,
   context->set_proxy_service(globals->proxy_script_fetcher_proxy_service.get());
   context->set_http_transaction_factory(
       globals->proxy_script_fetcher_http_transaction_factory.get());
-  context->set_ftp_transaction_factory(
-      globals->proxy_script_fetcher_ftp_transaction_factory.get());
   context->set_cookie_store(globals->system_cookie_store.get());
   context->set_server_bound_cert_service(
       globals->system_server_bound_cert_service.get());
@@ -217,8 +214,6 @@ ConstructSystemRequestContext(IOThread::Globals* globals,
   context->set_proxy_service(globals->system_proxy_service.get());
   context->set_http_transaction_factory(
       globals->system_http_transaction_factory.get());
-  context->set_ftp_transaction_factory(
-      globals->system_ftp_transaction_factory.get());
   context->set_cookie_store(globals->system_cookie_store.get());
   context->set_server_bound_cert_service(
       globals->system_server_bound_cert_service.get());
@@ -552,8 +547,6 @@ void IOThread::Init() {
       new net::HttpNetworkSession(session_params));
   globals_->proxy_script_fetcher_http_transaction_factory.reset(
       new net::HttpNetworkLayer(network_session));
-  globals_->proxy_script_fetcher_ftp_transaction_factory.reset(
-      new net::FtpNetworkLayer(globals_->host_resolver.get()));
 
   globals_->throttler_manager.reset(new net::URLRequestThrottlerManager());
   globals_->throttler_manager->set_net_log(net_log_);
@@ -917,8 +910,6 @@ void IOThread::InitSystemRequestContextOnIOThread() {
   globals_->system_http_transaction_factory.reset(
       new net::HttpNetworkLayer(
           new net::HttpNetworkSession(system_params)));
-  globals_->system_ftp_transaction_factory.reset(
-      new net::FtpNetworkLayer(globals_->host_resolver.get()));
   globals_->system_request_context.reset(
       ConstructSystemRequestContext(globals_, net_log_));
 

io_thread.h
@@ -122,8 +122,6 @@ class IOThread : public content::BrowserThreadDelegate {
     scoped_ptr<net::ProxyService> proxy_script_fetcher_proxy_service;
     scoped_ptr<net::HttpTransactionFactory>
         proxy_script_fetcher_http_transaction_factory;
-    scoped_ptr<net::FtpTransactionFactory>
-        proxy_script_fetcher_ftp_transaction_factory;
     scoped_ptr<net::URLRequestThrottlerManager> throttler_manager;
     scoped_ptr<net::URLSecurityManager> url_security_manager;
     // TODO(willchan): Remove proxy script fetcher context since it's not
@@ -135,7 +133,6 @@ class IOThread : public content::BrowserThreadDelegate {
     scoped_ptr<net::URLRequestContext> proxy_script_fetcher_context;
     scoped_ptr<net::ProxyService> system_proxy_service;
     scoped_ptr<net::HttpTransactionFactory> system_http_transaction_factory;
-    scoped_ptr<net::FtpTransactionFactory> system_ftp_transaction_factory;
     scoped_ptr<net::URLRequestContext> system_request_context;
     SystemRequestContextLeakChecker system_request_context_leak_checker;
     // |system_cookie_store| and |system_server_bound_cert_service| are shared

chromeos/drive/drive_url_request_job_unittest.cc
@@ -60,6 +60,10 @@ class TestURLRequestJobFactory : public net::URLRequestJobFactory {
     return url.is_valid() && IsHandledProtocol(url.scheme());
   }
 
+  virtual bool IsSafeRedirectTarget(const GURL& location) const OVERRIDE {
+    return true;
+  }
+
  private:
   const DriveURLRequestJob::FileSystemGetter file_system_getter_;
   scoped_refptr<base::SequencedTaskRunner> sequenced_task_runner_;

custom_handlers/protocol_handler_registry.cc
@@ -205,6 +205,12 @@ bool ProtocolHandlerRegistry::JobInterceptorFactory::IsHandledURL(
       job_factory_->IsHandledURL(url);
 }
 
+bool ProtocolHandlerRegistry::JobInterceptorFactory::IsSafeRedirectTarget(
+    const GURL& location) const {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  return job_factory_->IsSafeRedirectTarget(location);
+}
+
 // DefaultClientObserver ------------------------------------------------------
 
 ProtocolHandlerRegistry::DefaultClientObserver::DefaultClientObserver(

custom_handlers/protocol_handler_registry.h
@@ -114,6 +114,7 @@ class ProtocolHandlerRegistry : public ProfileKeyedService {
         net::NetworkDelegate* network_delegate) const OVERRIDE;
     virtual bool IsHandledProtocol(const std::string& scheme) const OVERRIDE;
     virtual bool IsHandledURL(const GURL& url) const OVERRIDE;
+    virtual bool IsSafeRedirectTarget(const GURL& location) const OVERRIDE;
 
    private:
     // When JobInterceptorFactory decides to pass on particular requests,

custom_handlers/protocol_handler_registry_unittest.cc
@@ -73,6 +73,9 @@ class FakeURLRequestJobFactory : public net::URLRequestJobFactory {
   virtual bool IsHandledURL(const GURL& url) const OVERRIDE {
     return false;
   }
+  virtual bool IsSafeRedirectTarget(const GURL& location) const OVERRIDE {
+    return true;
+  }
 };
 
 void AssertWillHandleIO(

extensions/api/web_request/web_request_api_unittest.cc
@@ -28,6 +28,7 @@
 #include "chrome/browser/extensions/api/web_request/web_request_api_helpers.h"
 #include "chrome/browser/extensions/event_router_forwarder.h"
 #include "chrome/browser/extensions/extension_warning_set.h"
+#include "chrome/browser/net/about_protocol_handler.h"
 #include "chrome/browser/net/chrome_network_delegate.h"
 #include "chrome/common/extensions/extension_messages.h"
 #include "chrome/common/extensions/features/feature.h"
@@ -36,6 +37,7 @@
 #include "chrome/test/base/testing_pref_service_syncable.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
+#include "content/public/common/url_constants.h"
 #include "content/public/test/test_browser_thread.h"
 #include "net/base/auth.h"
 #include "net/base/capturing_net_log.h"
@@ -44,6 +46,7 @@
 #include "net/base/upload_data_stream.h"
 #include "net/base/upload_file_element_reader.h"
 #include "net/dns/mock_host_resolver.h"
+#include "net/url_request/url_request_job_factory_impl.h"
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest-message.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -229,6 +232,12 @@ TEST_F(ExtensionWebRequestTest, BlockingEventPrecedenceRedirect) {
       &profile_, extension2_id, extension2_id, kEventName, kEventName + "/2",
       filter, ExtensionWebRequestEventRouter::ExtraInfoSpec::BLOCKING, -1, -1,
       ipc_sender_factory.GetWeakPtr());
+
+  net::URLRequestJobFactoryImpl job_factory;
+  job_factory.SetProtocolHandler(
+      chrome::kAboutScheme,
+      new chrome_browser_net::AboutProtocolHandler());
+  context_->set_job_factory(&job_factory);
 
   GURL redirect_url("about:redirected");
   GURL not_chosen_redirect_url("about:not_chosen");

net/about_protocol_handler.cc
@@ -16,4 +16,8 @@ net::URLRequestJob* AboutProtocolHandler::MaybeCreateJob(
   return new net::URLRequestAboutJob(request, network_delegate);
 }
 
+bool AboutProtocolHandler::IsSafeRedirectTarget(const GURL& location) const {
+  return false;
+}
+
 }  // namespace chrome_browser_net

commit 046dee9f7848ff8715748b14d7347213e78496a8

Allow specifying proxy scripts using file:, data:, and ftp: URLs.  This addresses a regression from r198915.
BUG=243974

Review URL: https://chromiumcodereview.appspot.com/15665006

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@203074 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

io_thread.cc
@@ -40,6 +40,7 @@
 #include "chrome/browser/policy/policy_service.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/host_mapping_rules.h"
 #include "net/base/net_util.h"
@@ -50,6 +51,7 @@
 #include "net/dns/host_cache.h"
 #include "net/dns/host_resolver.h"
 #include "net/dns/mapped_host_resolver.h"
+#include "net/ftp/ftp_network_layer.h"
 #include "net/http/http_auth_filter.h"
 #include "net/http/http_auth_handler_factory.h"
 #include "net/http/http_network_layer.h"
@@ -61,7 +63,11 @@
 #include "net/spdy/spdy_session.h"
 #include "net/ssl/default_server_bound_cert_store.h"
 #include "net/ssl/server_bound_cert_service.h"
+#include "net/url_request/data_protocol_handler.h"
+#include "net/url_request/file_protocol_handler.h"
+#include "net/url_request/ftp_protocol_handler.h"
 #include "net/url_request/url_fetcher.h"
+#include "net/url_request/url_request_job_factory_impl.h"
 #include "net/url_request/url_request_throttler_manager.h"
 #include "net/websockets/websocket_job.h"
 
@@ -188,6 +194,8 @@ ConstructProxyScriptFetcherContext(IOThread::Globals* globals,
   context->set_proxy_service(globals->proxy_script_fetcher_proxy_service.get());
   context->set_http_transaction_factory(
       globals->proxy_script_fetcher_http_transaction_factory.get());
+  context->set_job_factory(
+      globals->proxy_script_fetcher_url_request_job_factory.get());
   context->set_cookie_store(globals->system_cookie_store.get());
   context->set_server_bound_cert_service(
       globals->system_server_bound_cert_service.get());
@@ -547,6 +555,22 @@ void IOThread::Init() {
       new net::HttpNetworkSession(session_params));
   globals_->proxy_script_fetcher_http_transaction_factory.reset(
       new net::HttpNetworkLayer(network_session));
+  scoped_ptr<net::URLRequestJobFactoryImpl> job_factory(
+      new net::URLRequestJobFactoryImpl());
+  job_factory->SetProtocolHandler(chrome::kDataScheme,
+                                  new net::DataProtocolHandler());
+  job_factory->SetProtocolHandler(chrome::kFileScheme,
+                                  new net::FileProtocolHandler());
+#if !defined(DISABLE_FTP_SUPPORT)
+  globals_->proxy_script_fetcher_ftp_transaction_factory.reset(
+      new net::FtpNetworkLayer(globals_->host_resolver.get()));
+  job_factory->SetProtocolHandler(
+      chrome::kFtpScheme,
+      new net::FtpProtocolHandler(
+          globals_->proxy_script_fetcher_ftp_transaction_factory.get()));
+#endif
+  globals_->proxy_script_fetcher_url_request_job_factory =
+      job_factory.PassAs<net::URLRequestJobFactory>();
 
   globals_->throttler_manager.reset(new net::URLRequestThrottlerManager());
   globals_->throttler_manager->set_net_log(net_log_);

io_thread.h
@@ -57,6 +57,7 @@ class SSLConfigService;
 class TransportSecurityState;
 class URLRequestContext;
 class URLRequestContextGetter;
+class URLRequestJobFactory;
 class URLRequestThrottlerManager;
 class URLSecurityManager;
 }  // namespace net
@@ -122,6 +123,10 @@ class IOThread : public content::BrowserThreadDelegate {
     scoped_ptr<net::ProxyService> proxy_script_fetcher_proxy_service;
     scoped_ptr<net::HttpTransactionFactory>
         proxy_script_fetcher_http_transaction_factory;
+    scoped_ptr<net::FtpTransactionFactory>
+        proxy_script_fetcher_ftp_transaction_factory;
+    scoped_ptr<net::URLRequestJobFactory>
+        proxy_script_fetcher_url_request_job_factory;
     scoped_ptr<net::URLRequestThrottlerManager> throttler_manager;
     scoped_ptr<net::URLSecurityManager> url_security_manager;
     // TODO(willchan): Remove proxy script fetcher context since it's not

net/proxy_browsertest.cc
@@ -26,6 +26,25 @@
 
 namespace {
 
+// PAC script that sends all requests to an invalid proxy server.
+const base::FilePath::CharType kPACScript[] = FILE_PATH_LITERAL(
+    "bad_server.pac");
+
+// Verify kPACScript is installed as the PAC script.
+void VerifyProxyScript(Browser* browser) {
+  ui_test_utils::NavigateToURL(browser, GURL("http://google.com"));
+
+  // Verify we get the ERR_PROXY_CONNECTION_FAILED screen.
+  bool result = false;
+  EXPECT_TRUE(content::ExecuteScriptAndExtractBool(
+      browser->tab_strip_model()->GetActiveWebContents(),
+      "var textContent = document.body.textContent;"
+      "var hasError = textContent.indexOf('ERR_PROXY_CONNECTION_FAILED') >= 0;"
+      "domAutomationController.send(hasError);",
+      &result));
+  EXPECT_TRUE(result);
+}
+
 // This class observes chrome::NOTIFICATION_AUTH_NEEDED and supplies
 // the credential which is required by the test proxy server.
 // "foo:bar" is the required username and password for our test proxy server.
@@ -121,6 +140,115 @@ IN_PROC_BROWSER_TEST_F(ProxyBrowserTest, MAYBE_BasicAuthWSConnect) {
   const string16 result = watcher.WaitAndGetTitle();
   EXPECT_TRUE(EqualsASCII(result, "PASS"));
   EXPECT_TRUE(observer.auth_handled());
+}
+
+// Fetch PAC script via an http:// URL.
+class HttpProxyScriptBrowserTest : public InProcessBrowserTest {
+ public:
+  HttpProxyScriptBrowserTest()
+      : http_server_(net::SpawnedTestServer::TYPE_HTTP,
+                     net::SpawnedTestServer::kLocalhost,
+                     base::FilePath(FILE_PATH_LITERAL("chrome/test/data"))) {
+  }
+  virtual ~HttpProxyScriptBrowserTest() {}
+
+  virtual void SetUp() OVERRIDE {
+    ASSERT_TRUE(http_server_.Start());
+    InProcessBrowserTest::SetUp();
+  }
+
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    base::FilePath pac_script_path(FILE_PATH_LITERAL("files"));
+    command_line->AppendSwitchASCII(switches::kProxyPacUrl, http_server_.GetURL(
+        pac_script_path.Append(kPACScript).MaybeAsASCII()).spec());
+  }
+
+ private:
+  net::SpawnedTestServer http_server_;
+
+  DISALLOW_COPY_AND_ASSIGN(HttpProxyScriptBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(HttpProxyScriptBrowserTest, Verify) {
+  VerifyProxyScript(browser());
+}
+
+// Fetch PAC script via a file:// URL.
+class FileProxyScriptBrowserTest : public InProcessBrowserTest {
+ public:
+  FileProxyScriptBrowserTest() {}
+  virtual ~FileProxyScriptBrowserTest() {}
+
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    command_line->AppendSwitchASCII(switches::kProxyPacUrl,
+        ui_test_utils::GetTestUrl(
+            base::FilePath(base::FilePath::kCurrentDirectory),
+            base::FilePath(kPACScript)).spec());
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(FileProxyScriptBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(FileProxyScriptBrowserTest, Verify) {
+  VerifyProxyScript(browser());
+}
+
+// Fetch PAC script via an ftp:// URL.
+class FtpProxyScriptBrowserTest : public InProcessBrowserTest {
+ public:
+  FtpProxyScriptBrowserTest()
+      : ftp_server_(net::SpawnedTestServer::TYPE_FTP,
+                    net::SpawnedTestServer::kLocalhost,
+                    base::FilePath(FILE_PATH_LITERAL("chrome/test/data"))) {
+  }
+  virtual ~FtpProxyScriptBrowserTest() {}
+
+  virtual void SetUp() OVERRIDE {
+    ASSERT_TRUE(ftp_server_.Start());
+    InProcessBrowserTest::SetUp();
+  }
+
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    base::FilePath pac_script_path(kPACScript);
+    command_line->AppendSwitchASCII(
+        switches::kProxyPacUrl,
+        ftp_server_.GetURL(pac_script_path.MaybeAsASCII()).spec());
+  }
+
+ private:
+  net::SpawnedTestServer ftp_server_;
+
+  DISALLOW_COPY_AND_ASSIGN(FtpProxyScriptBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(FtpProxyScriptBrowserTest, Verify) {
+  VerifyProxyScript(browser());
+}
+
+// Fetch PAC script via a data: URL.
+class DataProxyScriptBrowserTest : public InProcessBrowserTest {
+ public:
+  DataProxyScriptBrowserTest() {}
+  virtual ~DataProxyScriptBrowserTest() {}
+
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    std::string contents;
+    // Read in kPACScript contents.
+    ASSERT_TRUE(file_util::ReadFileToString(ui_test_utils::GetTestFilePath(
+        base::FilePath(base::FilePath::kCurrentDirectory),
+        base::FilePath(kPACScript)),
+        &contents));
+    command_line->AppendSwitchASCII(switches::kProxyPacUrl,
+        std::string("data:,") + contents);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(DataProxyScriptBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(DataProxyScriptBrowserTest, Verify) {
+  VerifyProxyScript(browser());
 }
 
 }  // namespace