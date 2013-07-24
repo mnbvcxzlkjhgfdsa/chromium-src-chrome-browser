mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit e134b162ec46b6e7412d1f9605191b16868c25ad

History: Unchecked checkbox stays if it is clicked by a <label> click.
The behavior change by Blink r153039 caused this regression.

We don't want to focus on checkboxes in the history page. To avoid the
focus, we hook 'click' event for <label>, and change the state of an
associated checkbox, but don't focus on it.

BUG=260532

Review URL: https://chromiumcodereview.appspot.com/19506002

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@212474 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

resources/history/history.js
@@ -162,6 +162,7 @@ Visit.prototype.getResultDOM = function(propertyBag) {
     // Clicking anywhere in the entryBox will check/uncheck the checkbox.
     entryBox.setAttribute('for', checkbox.id);
     entryBox.addEventListener('mousedown', entryBoxMousedown);
+    entryBox.addEventListener('click', entryBoxClick);
   }
 
   // Keep track of the drop down that triggered the menu, so we know
@@ -1693,11 +1694,20 @@ function removeItems() {
  * @param {Event} e The click event.
  */
 function checkboxClicked(e) {
-  var checkbox = e.currentTarget;
+  handleCheckboxStateChange(e.currentTarget, e.shiftKey);
+}
+
+/**
+ * Post-process of checkbox state change. This handles range selection and
+ * updates internal state.
+ * @param {!HTMLInputElement} checkbox Clicked checkbox.
+ * @param {boolean} shiftKey true if shift key is pressed.
+ */
+function handleCheckboxStateChange(checkbox, shiftKey) {
   updateParentCheckbox(checkbox);
   var id = Number(checkbox.id.slice('checkbox-'.length));
   // Handle multi-select if shift was pressed.
-  if (event.shiftKey && (selectionAnchor != -1)) {
+  if (shiftKey && (selectionAnchor != -1)) {
     var checked = checkbox.checked;
     // Set all checkboxes from the anchor up to the clicked checkbox to the
     // state of the clicked one.
@@ -1756,6 +1766,21 @@ function entryBoxMousedown(event) {
   if (event.shiftKey) {
     event.preventDefault();
   }
+}
+
+/**
+ * Handle click event for entryBox labels.
+ * @param {!MouseEvent} event A click event.
+ */
+function entryBoxClick(event) {
+  var tagName = event.target.tagName;
+  if (tagName == 'BUTTON' || tagName == 'INPUT' || tagName == 'A')
+    return;
+  var checkbox = event.currentTarget.control;
+  checkbox.checked = !checkbox.checked;
+  handleCheckboxStateChange(checkbox, event.shiftKey);
+  // We don't want to focus on the checkbox.
+  event.preventDefault();
 }
 
 // This is pulled out so we can wait for it in tests.

commit 6a71629a6c754d4f749b2105abb4600a291191c5

REGRESSION: A history item should not be checked when the bookmark star is clicked.
This is a regression by r212474.
preventDefault() is called for a click event on the bookmark star,
However we manually check the checkbox by entryBox click and ignore the
|defaultPrevented| state.  We should repect it.

BUG=262869

Review URL: https://chromiumcodereview.appspot.com/19771022

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@213351 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

resources/history/history.js
@@ -1772,6 +1772,9 @@ function entryBoxMousedown(event) {
  * @param {!MouseEvent} event A click event.
  */
 function entryBoxClick(event) {
+  // Do nothing if a bookmark star is clicked.
+  if (event.defaultPrevented)
+    return;
   var tagName = event.target.tagName;
   if (tagName == 'BUTTON' || tagName == 'INPUT' || tagName == 'A')
     return;