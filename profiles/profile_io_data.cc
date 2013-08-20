mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit 0f8b715a4a8b3b0a9c29748dd2d35e54de6f582e

Remove WorkerPool dependency from URLRequestFileJob.
Make net::URLRequestFileJob pass a TaskRunner to FileStream, instead of using a deprecated FileStream constructor that depends on WorkerPool. This TaskRunner is obtained from the blocking pool where possible. Also, remove two explicit uses of WorkerPool in net/url_request/ and chrome/browser/.

Removes dependency on deprecated code. Also fixes memory leaks in unit tests caused by FileStream destruction tasks not running to completion in WorkerPool.

BUG=248513, 251774

Review URL: https://chromiumcodereview.appspot.com/22795006

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@218273 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

profiles/profile_io_data.cc
@@ -19,6 +19,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/content_settings/content_settings_provider.h"
@@ -835,7 +836,11 @@ scoped_ptr<net::URLRequestJobFactory> ProfileIOData::SetUpJobFactoryDefaults(
   // NOTE(willchan): Keep these protocol handlers in sync with
   // ProfileIOData::IsHandledProtocol().
   bool set_protocol = job_factory->SetProtocolHandler(
-      chrome::kFileScheme, new net::FileProtocolHandler());
+      chrome::kFileScheme,
+      new net::FileProtocolHandler(
+          content::BrowserThread::GetBlockingPool()->
+              GetTaskRunnerWithShutdownBehavior(
+                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)));
   DCHECK(set_protocol);
 
   DCHECK(extension_info_map_.get());

extensions/extension_protocols.cc
@@ -19,8 +19,8 @@
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_restrictions.h"
-#include "base/threading/worker_pool.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_info_map.h"
 #include "chrome/browser/extensions/image_loader.h"
@@ -35,6 +35,7 @@
 #include "chrome/common/extensions/manifest_url_handler.h"
 #include "chrome/common/extensions/web_accessible_resources_handler.h"
 #include "chrome/common/url_constants.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/resource_request_info.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_resource.h"
@@ -136,7 +137,7 @@ class URLRequestResourceBundleJob : public net::URLRequestSimpleJob {
 
     std::string* read_mime_type = new std::string;
     bool* read_result = new bool;
-    bool posted = base::WorkerPool::PostTaskAndReply(
+    bool posted = content::BrowserThread::PostBlockingPoolTaskAndReply(
         FROM_HERE,
         base::Bind(&ReadMimeTypeFromFile, filename_,
                    base::Unretained(read_mime_type),
@@ -146,8 +147,7 @@ class URLRequestResourceBundleJob : public net::URLRequestSimpleJob {
                    mime_type, charset, data,
                    base::Owned(read_mime_type),
                    base::Owned(read_result),
-                   callback),
-        true /* task is slow */);
+                   callback));
     DCHECK(posted);
 
     return net::ERR_IO_PENDING;
@@ -251,7 +251,11 @@ class URLRequestExtensionJob : public net::URLRequestFileJob {
                          const base::FilePath& relative_path,
                          const std::string& content_security_policy,
                          bool send_cors_header)
-    : net::URLRequestFileJob(request, network_delegate, base::FilePath()),
+    : net::URLRequestFileJob(
+          request, network_delegate, base::FilePath(),
+          content::BrowserThread::GetBlockingPool()->
+              GetTaskRunnerWithShutdownBehavior(
+                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)),
       // TODO(tc): Move all of these files into resources.pak so we don't break
       // when updating on Linux.
       resource_(extension_id, directory_path, relative_path),
@@ -267,7 +271,7 @@ class URLRequestExtensionJob : public net::URLRequestFileJob {
   virtual void Start() OVERRIDE {
     base::FilePath* read_file_path = new base::FilePath;
     base::Time* last_modified_time = new base::Time();
-    bool posted = base::WorkerPool::PostTaskAndReply(
+    bool posted = content::BrowserThread::PostBlockingPoolTaskAndReply(
         FROM_HERE,
         base::Bind(&ReadResourceFilePathAndLastModifiedTime, resource_,
                    base::Unretained(read_file_path),
@@ -275,8 +279,7 @@ class URLRequestExtensionJob : public net::URLRequestFileJob {
         base::Bind(&URLRequestExtensionJob::OnFilePathAndLastModifiedTimeRead,
                    weak_factory_.GetWeakPtr(),
                    base::Owned(read_file_path),
-                   base::Owned(last_modified_time)),
-        true /* task is slow */);
+                   base::Owned(last_modified_time)));
     DCHECK(posted);
   }
 

extensions/extension_resource_protocols.cc
@@ -7,6 +7,7 @@
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "base/path_service.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_checker.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/extensions/extension_file_util.h"
@@ -25,9 +26,12 @@ class ExtensionResourcesJob : public net::URLRequestFileJob {
  public:
   ExtensionResourcesJob(net::URLRequest* request,
                         net::NetworkDelegate* network_delegate)
-    : net::URLRequestFileJob(request, network_delegate, base::FilePath()),
-      weak_ptr_factory_(this) {
-  }
+      : net::URLRequestFileJob(
+            request, network_delegate, base::FilePath(),
+            content::BrowserThread::GetBlockingPool()->
+                GetTaskRunnerWithShutdownBehavior(
+                    base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)),
+        weak_ptr_factory_(this) {}
 
   virtual void Start() OVERRIDE;
 

io_thread.cc
@@ -20,6 +20,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread.h"
 #include "base/threading/worker_pool.h"
 #include "base/time/default_tick_clock.h"
@@ -594,8 +595,12 @@ void IOThread::InitAsync() {
       new net::URLRequestJobFactoryImpl());
   job_factory->SetProtocolHandler(chrome::kDataScheme,
                                   new net::DataProtocolHandler());
-  job_factory->SetProtocolHandler(chrome::kFileScheme,
-                                  new net::FileProtocolHandler());
+  job_factory->SetProtocolHandler(
+      chrome::kFileScheme,
+      new net::FileProtocolHandler(
+          content::BrowserThread::GetBlockingPool()->
+              GetTaskRunnerWithShutdownBehavior(
+                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)));
 #if !defined(DISABLE_FTP_SUPPORT)
   globals_->proxy_script_fetcher_ftp_transaction_factory.reset(
       new net::FtpNetworkLayer(globals_->host_resolver.get()));

net/load_timing_browsertest.cc
@@ -13,6 +13,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/strings/stringprintf.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_paths.h"
@@ -119,10 +120,13 @@ class MockUrlRequestJobWithTiming : public net::URLRequestFileJob {
                               net::NetworkDelegate* network_delegate,
                               const base::FilePath& path,
                               const TimingDeltas& load_timing_deltas)
-      : net::URLRequestFileJob(request, network_delegate, path),
+      : net::URLRequestFileJob(
+            request, network_delegate, path,
+            content::BrowserThread::GetBlockingPool()->
+                GetTaskRunnerWithShutdownBehavior(
+                    base::SequencedWorkerPool::SKIP_ON_SHUTDOWN)),
         load_timing_deltas_(load_timing_deltas),
-        weak_factory_(this) {
-  }
+        weak_factory_(this) {}
 
   // net::URLRequestFileJob implementation:
   virtual void Start() OVERRIDE {

commit 1d07c24cc9e7dd00aff6ef1e28d68efe1f7eaaeb

Fix compile error introduced by r218273 when debug_devtools=1.
r218273 changed the constructor of URLRequestFileJob, but one line in
profile_io_data.cc still used the old form. This CL fixes that.

BUG=NONE
TBR=mmenke
R=pfeldman
NOTRY=true

Review URL: https://chromiumcodereview.appspot.com/23283013

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@218452 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

profiles/profile_io_data.cc
@@ -210,7 +210,11 @@ class DebugDevToolsInterceptor
       net::NetworkDelegate* network_delegate) const OVERRIDE {
     base::FilePath path;
     if (IsSupportedDevToolsURL(request->url(), &path))
-      return new net::URLRequestFileJob(request, network_delegate, path);
+      return new net::URLRequestFileJob(
+          request, network_delegate, path,
+          content::BrowserThread::GetBlockingPool()->
+              GetTaskRunnerWithShutdownBehavior(
+                  base::SequencedWorkerPool::SKIP_ON_SHUTDOWN));
 
     return NULL;
   }