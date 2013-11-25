mnbvcxzlkjhgfdsa/chromium-src-chrome-browser

commit 5681c84c8d08cf2c5664678bb77bb7aabfe1c1b8

Remove the RenderViewHostObserver for content settings. RenderViewForInterstitialPageCreated was actually incorrectly called by content, since it should have been called when the RenderView is created (to match RenderViewCreated callback), and not the RenderViewHost.
Also remove the overriding of RenderViewForInterstitialPageCreated in android_webview, which doesn't have interstitials.

BUG=306569
R=jochen@chromium.org, mkosiba@chromium.org

Review URL: https://codereview.chromium.org/29393004

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@229819 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

content_settings/tab_specific_content_settings.cc
@@ -29,7 +29,6 @@
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_view_host.h"
-#include "content/public/browser/render_view_host_observer.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "net/cookies/canonical_cookie.h"
@@ -42,22 +41,6 @@ using content::RenderViewHost;
 using content::WebContents;
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(TabSpecificContentSettings);
-
-namespace {
-
-class InterstitialHostObserver : public content::RenderViewHostObserver {
- public:
-  explicit InterstitialHostObserver(RenderViewHost* rvh)
-      : content::RenderViewHostObserver(rvh) {}
-
-  // content::RenderViewHostObserver overrides.
-  virtual void RenderViewHostInitialized() OVERRIDE {
-    Send(new ChromeViewMsg_SetAsInterstitial(routing_id()));
-    delete this;
-  }
-};
-
-}  // namespace
 
 TabSpecificContentSettings::SiteDataObserver::SiteDataObserver(
     TabSpecificContentSettings* tab_specific_content_settings)
@@ -664,8 +647,8 @@ void TabSpecificContentSettings::SetPepperBrokerAllowed(bool allowed) {
 void TabSpecificContentSettings::RenderViewForInterstitialPageCreated(
     RenderViewHost* render_view_host) {
   // We want to tell the renderer-side code to ignore content settings for this
-  // page but we must wait until the RenderView is created.
-  new InterstitialHostObserver(render_view_host);
+  // page.
+  Send(new ChromeViewMsg_SetAsInterstitial(routing_id()));
 }
 
 bool TabSpecificContentSettings::OnMessageReceived(

commit 42030454f10289848d48d147e8cd0e8eeaae4bd1

Fix interstitials being broken when JavaScript is disabled.
This was a regression from r229819.

BUG=322948
R=felt@chromium.org, rsleevi@chromium.org, felt

Review URL: https://codereview.chromium.org/85773003

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@237184 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

content_settings/tab_specific_content_settings.cc
@@ -676,7 +676,8 @@ void TabSpecificContentSettings::RenderViewForInterstitialPageCreated(
     RenderViewHost* render_view_host) {
   // We want to tell the renderer-side code to ignore content settings for this
   // page.
-  Send(new ChromeViewMsg_SetAsInterstitial(routing_id()));
+  render_view_host->Send(new ChromeViewMsg_SetAsInterstitial(
+      render_view_host->GetRoutingID()));
 }
 
 bool TabSpecificContentSettings::OnMessageReceived(

ssl/ssl_browser_tests.cc
@@ -1640,6 +1640,31 @@ IN_PROC_BROWSER_TEST_F(SSLUITestIgnoreCertErrors, TestWSS) {
   EXPECT_TRUE(LowerCaseEqualsASCII(result, "pass"));
 }
 
+// Verifies that if JavaScript is disabled interstitials aren't affected.
+// http://crbug.com/322948
+IN_PROC_BROWSER_TEST_F(SSLUITest, InterstitialNotAffectedByContentSettings) {
+  browser()->profile()->GetHostContentSettingsMap()->SetDefaultContentSetting(
+      CONTENT_SETTINGS_TYPE_JAVASCRIPT, CONTENT_SETTING_BLOCK);
+
+  ASSERT_TRUE(https_server_expired_.Start());
+  WebContents* tab = browser()->tab_strip_model()->GetActiveWebContents();
+  ui_test_utils::NavigateToURL(browser(),
+      https_server_expired_.GetURL("files/ssl/google.html"));
+  CheckAuthenticationBrokenState(tab, net::CERT_STATUS_DATE_INVALID, false,
+                                 true);  // Interstitial showing
+
+  InterstitialPage* interstitial_page = tab->GetInterstitialPage();
+  content::RenderViewHost* interstitial_rvh =
+      interstitial_page->GetRenderViewHostForTesting();
+  bool result = false;
+  ASSERT_TRUE(content::ExecuteScriptAndExtractBool(
+              interstitial_rvh,
+              "window.domAutomationController.send(true);",
+              &result));
+  // The above will hang without the fix.
+  ASSERT_TRUE(result);
+}
+
 // TODO(jcampan): more tests to do below.
 
 // Visit a page over https that contains a frame with a redirect.